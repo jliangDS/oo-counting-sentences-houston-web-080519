22ae2f6ff4743eeb789be3a8dbc7e1b09ba2655c
Jun Liang <illuminatemynight@gmail.com>
2019-08-09 16:01:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
 require 'pry'
-
 class String
 
   def sentence?
-    
+    if self.include?('.')
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def question?
-
+    if self.include?('?')
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def exclamation?
-
+    if self.include?('!')
+      return true 
+    else 
+      return false 
+    end 
   end
-
+  
   def count_sentences
-
-  end
+    
+  
+    return @count_of_sentence 
+  end 
+  
 end
